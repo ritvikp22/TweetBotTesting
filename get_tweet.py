8850fc4ccfcf5fc32e5dd3524854ffe9096c3a47
Ritvik Pandey <ritvikp22@gmail.com>
2020-07-02 23:36:29 -0700

    choice of user

--- get_tweet.py
+++ get_tweet.py
@@ -13,10 +13,14 @@ api = tweepy.API(auth)
 # Arbitrary username
 test = api.get_user("elonmusk")
 
+#Choose username
+choice = input("Whose tweets would you like to see")
+
 # Function to return n number of tweets from a specified user
 def get_tweet(num_tweets):
-    for status in tweepy.Cursor(api.user_timeline, screen_name="elonmusk", tweet_mode="extended").items(num_tweets):
+    for status in tweepy.Cursor(api.user_timeline, screen_name= choice, tweet_mode="extended").items(num_tweets):
         print(status.full_text)
 
 #Running Function
-get_tweet(10)
+num = 10
+get_tweet(num)
